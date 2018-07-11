aedf09c20e491603d0b82b746b29e1565059574f
jsta <stachel2@msu.edu>
2018-07-11 17:48:11 -0400

    stash testing file

---
+++ test.py
@@ -0,0 +1,14 @@
+day = '2018-06-26'
+day = '2018-04-11'
+day = '2018-04-10'
+
+posts = get_posts(day)
+res = pd.DataFrame(posts)
+res.columns = ['title', 'summary', 'prism_url', 'dc_source', 'updated']
+res['updated'] = pd.to_datetime(res['updated'])
+res = res.sort_values(by = ['updated'])
+res = filter_limno(res)
+res.to_csv("test.csv")
+res = filter_today(res, day)
+
+get_papers(day)
