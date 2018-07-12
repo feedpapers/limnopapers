ba8819ce282465f3616ad5000bfa50def625af23
jsta <stachel2@msu.edu>
2018-07-11 21:19:58 -0400

    add wrr, closes #2

--- limnopapers.py
+++ limnopapers.py
@@ -19,7 +19,8 @@ def get_posts(day = str(datetime.date.today())):
         'http://onlinelibrary.wiley.com/rss/journal/10.1002/(ISSN)2378-2242',        
         'https://www.journals.uchicago.edu/action/showFeed?type=etoc&feed=rss&jc=fws',
         'https://www.tandfonline.com/action/showFeed?type=etoc&feed=rss&jc=tinw20',
-        'http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1365-2427'        
+        'http://onlinelibrary.wiley.com/rss/journal/10.1111/(ISSN)1365-2427',
+        'http://onlinelibrary.wiley.com/rss/journal/10.1002/(ISSN)1944-7973'        
         ]
 
     posts = []
