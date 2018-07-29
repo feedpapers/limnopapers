4c241295404da12a83d7017b598c46615d2aea64
jsta <stachel2@msu.edu>
2018-07-29 18:03:16 -0400

    organize tests

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -3,18 +3,22 @@ from limnopapers import *
 day = '2018-06-26'
 day = '2018-04-11'
 day = '2018-04-10'
+day = '2018-07-27'
 
+# ---- test full limnotoots ----
+data = get_papers(day)        
+toots = data['title'] + ". " + data['dc_source']  + ". " + data['prism_url']
+toots.to_csv("test.csv")
+
+# ---- test limnotoots guts ----
 posts = get_posts(day)
 res = pd.DataFrame(posts)
-res.to_csv("test.csv")
+# res.to_csv("test.csv")
 
 res.columns = ['title', 'summary', 'prism_url', 'dc_source', 'updated']
 res['updated'] = pd.to_datetime(res['updated'])
 res = res.sort_values(by = ['updated'])
 res = filter_limno(res)
-res.to_csv("test.csv")
 res = filter_today(res, day)
-
-get_papers(day)
-
+res.to_csv("test.csv")
 
