8e9c0a656bdba39c5f6675345fd7d00f93f62e5d
jsta <stachel2@msu.edu>
2018-09-18 08:49:10 -0400

    linting

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -1,13 +1,17 @@
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-
-from limnopapers import *
+import os
+import sys
+import inspect
 import feedparser
 import pandas as pd
 
-url = "https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=10021&channel-name=Ecosystems"
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from limnopapers import *
+
+url = "http://onlinelibrary.wiley.com/rss/journal/10.1002/(ISSN)2169-8961"
 posts = []
 
 feed = feedparser.parse(url)
@@ -16,11 +20,11 @@ for post in feed.entries:
 
 res = pd.DataFrame(posts)
 try:
-    if(len(set(list(res.columns)). \
+    if(len(set(list(res.columns)).
            intersection(['title', 'link', 'published'])) != 3):
         raise ValueError('Missing Field')
 except:
-    if(len(set(list(res.columns)). \
+    if(len(set(list(res.columns)).
            intersection(['title', 'link', 'updated'])) != 3):
         raise ValueError('Missing Field')
 
