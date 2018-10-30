9de309b0ada3f91f2729a0045a3f80fc08986309
jsta <stachel2@msu.edu>
2018-10-30 15:42:04 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""A package to monitor limnology RSS feeds and tweet new articles."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="limnopapers",
+    version="0.0.1",
+    author="Joseph Stachelek",
+    author_email="stachel2@msu.edu",
+    description="A package to monitor limnology RSS feeds and tweet new \
+                articles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsta/limnopapers",
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
