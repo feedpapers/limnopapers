780f00e6baaba4fc54d225a81bd3c411d58d2461
jsta <stachel2@msu.edu>
2018-10-30 16:05:10 -0400

    work-out optional arguments

--- tests/test_cmdarg.py
+++ tests/test_cmdarg.py
@@ -1,7 +1,6 @@
-import sys
-
-if(len(sys.argv) == 2):
-    print(sys.argv[1])
-else:
-    print("no argument")
-
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--square', help="display a square of a given number",
+                    type=int, default = 2)
+args = parser.parse_args()
+print(args.square**2)
