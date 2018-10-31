e102e5c918c083f02e4e8020aa8e135ee764a530
jsta <stachel2@msu.edu>
2018-10-31 10:05:56 -0400

    attempt import path fix2

--- tests/test_feed.py
+++ tests/test_feed.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 import importlib.util
 spec = importlib.util.spec_from_file_location("limnopapers",
-                                              "../limnopapers/limnopapers.py")
+                                              "limnopapers/limnopapers.py")
 limnopapers = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(limnopapers)
 
