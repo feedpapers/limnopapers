4e93ff2c004596d2aa2d9a1c77ef6d266f81c407
jsta <stachel2@msu.edu>
2018-12-11 08:08:31 -0500

    ignore junk

--- limnopapers/limnopapers.py
+++ limnopapers/limnopapers.py
@@ -59,7 +59,8 @@ def filter_limno(df):
                       'mud carp', 'groundwater status', 'water system planner',
                       'agribusiness', 'amplicon', 'gene expression',
                       '16S rRNA', 'Enterococcus', 'lead service line',
-                      'Great Lakes']
+                      'nesting', 'hatching', 'and fish',
+                      'environmental literacy']
     has_junk_summary = ~df['summary'].str.contains('|'.join(filter_against),
                                                    case = False)
     has_junk_title = ~df['title'].str.contains('|'.join(filter_against),
