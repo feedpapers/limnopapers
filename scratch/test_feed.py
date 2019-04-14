8aab4f615e75e5fee1814b49febc1481ef3842b8
jsta <stachel2@msu.edu>
2019-04-14 09:44:49 -0400

    add peerj, closes #62

--- scratch/test_feed.py
+++ scratch/test_feed.py
@@ -11,7 +11,7 @@ spec = importlib.util.spec_from_file_location("limnopapers",
 limnopapers = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(limnopapers)
 
-url = "http://onlinelibrary.wiley.com/rss/journal/10.1002/(ISSN)2150-8925"
+url = "https://peerj.com/articles/index.rss2"
 posts = []
 
 feed = fp.parse(url)
