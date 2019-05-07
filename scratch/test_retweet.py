75ffbe0e6e7d2cff3619cb5e848e4ce384d15ffe
jsta <stachel2@msu.edu>
2019-05-07 16:33:35 -0400

    stash retweet script

---
+++ scratch/test_retweet.py
@@ -0,0 +1,27 @@
+import twitter
+import pandas as pd
+import limnopapers as lp
+try:
+    import config
+except:
+    print("No twitter keys found")
+
+
+api = twitter.Api(consumer_key=config.consumer_key,
+                  consumer_secret=config.consumer_secret,
+                  access_token_key = config.access_token_key,
+                  access_token_secret=config.access_token_secret)
+
+user = "JEnvironQual"
+statuses = api.GetUserTimeline(screen_name = user)
+
+res = []
+for p in statuses:
+    res.append({'title': p.text, 'summary': p.text, 'id': p.id})
+res = pd.DataFrame(res)
+
+res_limno = lp.filter_limno(res)
+[statuses[i] for i in res_limno['papers']['index']]
+
+# retweet 
+api.PostRetweet()
