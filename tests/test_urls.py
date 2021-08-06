b6643490b72340c17af52dda0762774e125366de
jsta <stachel2@msu.edu>
2021-08-06 16:01:16 -0600

    update user agent, format with black [skip ci]

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -9,24 +9,27 @@ import errno
 
 def url_ok(url):
     # https://stackoverflow.com/a/43167631/3362993
+    # url = "https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=267&channel-name=Environmental+Management"
     headers = requests.utils.default_headers()
-    headers['User-Agent'] = ("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:67.0)"
-                             " Gecko/20100101"
-                             " Firefox/67.0")
+    headers["User-Agent"] = (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0)"
+        " Gecko/20100101"
+        " Firefox/90.0"
+    )
     try:
-        r = requests.head(url, headers = headers, timeout = 10)
+        r = requests.head(url, headers=headers, timeout=10)
         return r.status_code in [200, 301, 403]
+        response = requests.get(someurl)
     except:
         print(url)
         return False
 
 
 def test_urls():
-    rawrss = pd.read_csv(pkg_resources.resource_filename('limnopapers',
-                                                         'journals.csv'))
+    rawrss = pd.read_csv(pkg_resources.resource_filename("limnopapers", "journals.csv"))
     url_statuses = []
     for i in range(len(rawrss.index)):
-        url_statuses.append(url_ok(rawrss['rawrss'][i]))
+        url_statuses.append(url_ok(rawrss["rawrss"][i]))
 
-    print(rawrss[[not i for i in url_statuses]]['rawrss'])
+    print(rawrss[[not i for i in url_statuses]]["rawrss"])
     assert all(url_statuses) is True
