c260b87df76a7695417ea1e2a29b8cf971a834ba
jsta <stachel2@msu.edu>
2023-03-13 14:03:10 -0600

    fight with assets

--- dashboard.py
+++ dashboard.py
@@ -145,7 +145,7 @@ def main():
     app = Dash(__name__)
     app.scripts.config.serve_locally = True
     app.css.config.serve_locally = True
-    app.config.update({"requests_pathname_prefix": "/limnopapers/"})
+    # app.config.update({"requests_pathname_prefix": "/limnopapers/"})
 
     app.layout = html.Div(
         children=[
