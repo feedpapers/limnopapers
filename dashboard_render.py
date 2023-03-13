9b1abf105e3dbcaeddd1e8e51e0d540399b2ca31
jsta <stachel2@msu.edu>
2023-03-13 13:07:17 -0600

    battle with dashboard env

---
+++ dashboard_render.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+driver.get("http://127.0.0.1:9050/")
+time.sleep(5)  # Let the user actually see something!
+print(driver.title)
+driver.quit()
